28d4f36a742d9fdc3ffea56b7e59afe459d923b0
Jordan Kelly <56368044+JordanK37@users.noreply.github.com>
2022-10-22 22:30:36 -0600

    Add files via upload

--- LAMP-Part-4-main/php/add.php
+++ LAMP-Part-4-main/php/add.php
@@ -59,7 +59,7 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
-    echo "Name, Country, and time: " . $row["first", "last", "country", "time"] . "<br>" ;
+    echo "Name: " . $row["first"] . "<br>" ;
   }
 } else {
   echo "0 results";
